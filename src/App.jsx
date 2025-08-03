2065378b8ce1afb075bcdd4312978a9ebddfd98c
samviswa03 <sangili2025g@gmail.com>
2025-08-03 19:13:06 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import Footer from './footer';
-import waren from './assets/wbufet.png';
+// import waren from './assets/wbufet.png';
 import chatui from '../src/assets/chatui.png';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
           <div className='sticky mt-56 p-4 flex flex-col w-full md:w-full'>
             <h1 className='font-bold text-2xl md:text-4xl'>Chat app</h1>
             <div className='flex items-center gap-5 md:justify-between w-full mt-3'>
-              <img src={waren} className='w-16 h-16 md:h-24 md:w-24 rounded-full' alt="logo" />
+              <img src="https://i.postimg.cc/6qnNKVzc/wbufet.jpg" className='w-16 h-16 md:h-24 md:w-24 rounded-full' alt="logo" />
               <p className='md:text-xl' >Waran Bufet</p>
               <p className='md:text-xl'>Available to work</p>
               <FontAwesomeIcon icon={faHeart} className=" text-pink-600 outline-2 outline-gray-300  p-1 rounded-full text-lg" />
